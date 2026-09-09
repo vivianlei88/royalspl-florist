a14d5c9508876703f52f9944461e0364a8dba540
vivianlei88 <vivianlei88@users.noreply.github.com>
2026-09-10 06:51:21 +0800

    feat: 添加Service Worker和完善PWA配置，支持添加到手机桌面

---
+++ admin-sw.js
@@ -0,0 +1,43 @@
+// RoyalSpl 後台管理 Service Worker
+const CACHE_NAME = 'royalspl-admin-v1';
+const urlsToCache = [
+  '/admin.html',
+  '/admin-manifest.json',
+  '/apple-touch-icon.png',
+  '/favicon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
